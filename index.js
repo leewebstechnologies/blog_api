268e366dc9a803598d7e66c21be0f3821b6286b9
leewebstechnologies <aliuajibade317@gmail.com>
2023-08-30 23:51:16 +0100

    users completed

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const cors = require("cors");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -17,6 +17,7 @@ mongoose
   });
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running!");
