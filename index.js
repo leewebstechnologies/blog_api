09e0196593768c5a916d68a2ab46f72518703762
leewebstechnologies <aliuajibade317@gmail.com>
2023-09-06 09:34:54 +0100

    path in axios instance corrected

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const multer = require("multer");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(console.log("Connected to Mongo DB"))
-  .catch((error) => {
-    console.log(error);
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
   });
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
+  destination: (req, file, cb) => {
+    cb(null, "images");
   },
-  filename: (req, file, callback) => {
-    callback(null, req.body.name);
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
   },
 });
 
@@ -40,6 +41,10 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-  console.log("Backend is running!");
+app.get("/", (req, res) => {
+  return res.status(200).json({ msg: "Welcome to homepage" });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
 });
